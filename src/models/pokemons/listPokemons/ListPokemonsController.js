9d0f04e5a6b431729f6aca5cd7f76f9a515d7555
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2022-09-03 10:41:10 -0400

    Removing second API call from class ListPokemonsUseCase and adding it to class ListPokemonsCOntroller.

--- src/models/pokemons/listPokemons/ListPokemonsController.js
+++ src/models/pokemons/listPokemons/ListPokemonsController.js
@@ -1,12 +1,27 @@
 class LisPokemonsController {
   async handle(request, response) {
     const ListPokemonsUseCase = require("./ListPokemonsUseCase");
+    const urlAllPokemons = "/pokemon?limit=10&offset=0";
+    const allPokemonsInfo = [];
 
     const listPokemonsUseCase = new ListPokemonsUseCase();
 
-    const allPokemons = await listPokemonsUseCase.list();
+    const allPokemons = await listPokemonsUseCase.list(urlAllPokemons);
 
-    return response.json({ pokemons: allPokemons });
+    for (let i = 0; i < allPokemons.data.results.length; i++) {
+      let id = allPokemons.data.results[i].url.split("/");
+      const urlPokemonInfo = `/pokemon-form/${id[id.length - 2]}`;
+
+      let pokemon = await listPokemonsUseCase.list(urlPokemonInfo);
+
+      allPokemonsInfo.push({
+        pokemon_id: pokemon.data.id,
+        pokemon_name: pokemon.data.pokemon.name,
+        pokemon_image: pokemon.data.sprites.front_default,
+      });
+    };
+
+    return response.json({ pokemons: allPokemonsInfo });
   }
 }
 
--- src/models/pokemons/listPokemons/ListPokemonsUseCase.js
+++ src/models/pokemons/listPokemons/ListPokemonsUseCase.js
@@ -3,21 +3,10 @@ const api = require("../../../services/api");
 class ListPokemosnUseCase {
   pokemons = [];
 
-  async list() {
-    let list = await api.get("/pokemon?limit=10&offset=0");
-    let allPokemons = [];
+  async list(url) {
+    let list = await api.get(url);
 
-    for (let i = 0; i < list.data.results.length; i++) {
-      let id = list.data.results[i].url.split("/");
-      let pokemon = await api.get(`/pokemon-form/${id[id.length - 2]}`);
-      allPokemons.push({
-        pokemon_id: pokemon.data.id,
-        pokemon_name: pokemon.data.pokemon.name,
-        pokemon_image: pokemon.data.sprites.front_default,
-      });
-    };
-
-    return allPokemons;
+    return list;
   }
 }
 
